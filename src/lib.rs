b67650907256a9ee58af224796b5f9c8233bdb0b
uriegel <uriegel@hotmail.de>
2021-05-21 16:46:11 +0200

    TODO

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,7 @@ pub fn test() {
     println!("Very early hello example test");
 }
 
+// TODO GTK window app
+// TODO Linux windows dependant sources
+// TODO Windoes Window app
+// TODO add webviews
