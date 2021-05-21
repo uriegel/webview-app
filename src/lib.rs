dfc67f82709ba2301dac7399cc04a9bbf843953b
uriegel <uriegel@hotmail.de>
2021-05-21 19:39:00 +0200

    TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,10 @@ pub fn test() {
     println!("Very early hello example test");
 }
 
+// TODO GTK: When there is no Glade.main => building window with webview
+// TODO GTK: When there is a Glade.main => building window with webview and main.glade, hello_glade example
 // TODO GTK: Extending Settings 
+// TODO GTK: Mainwindow (HeaderBar -> hello_header example, Subclass MainWindow with new)
 // TODO GTK: Properties like window title
 // TODO Linux windows dependant sources
 // TODO Windoes Window app
