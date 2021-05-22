826c1e166ee6d55b3689608f7c6ec69d78d327e5
uriegel <uriegel@hotmail.de>
2021-05-22 18:15:59 +0200

    Todo

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,11 @@ pub mod app;
 mod settings;
 mod warp_server;
 
+// TODO Warp filters, add to AppSettings,alternative: add in initialization callback
+// TODO Warp methods like sendFile, sendJson...
+// TODO on_msg_from_javascript
+// TODO inject javascript on load
+
 // TODO GTK: glade: connect headerbar
 // TODO GTK: Mainwindow (HeaderBar -> hello_header example, Subclass MainWindow with new)
 // TODO Inject global.js, Messages to and from the webview
