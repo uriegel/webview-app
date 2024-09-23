7a7182ce7f84d55461f85ff814087f65ecb42e1e
Uwe Riegel <uriegel@hotmail.de>
2024-09-24 00:59:19 +0200

    windows utf16 strings and free

--- WebViewApp/WebViewApp/Test.cpp
+++ WebViewApp/WebViewApp/Test.cpp
@@ -1,6 +1,18 @@
 #include "pch.h"
 
-void __stdcall Test1()
-{
-    MessageBoxW(NULL, L"Text", L"Cäptschn", MB_OK);
+wchar_t* __stdcall Test1(wchar_t* text_to_display) {
+    MessageBoxW(NULL, text_to_display, L"Cäptschn", MB_OK);
+    auto txt = L"Das ist ein schöner Result";
+    auto len = wcslen(txt);
+    auto text = new wchar_t[len + 1];
+    wcscpy_s(text, len + 1, txt);
+    return text;
+}
+
+size_t Strlen(const wchar_t* txt_ptr) {
+    return wcslen(txt_ptr);
+}
+
+void Free(wchar_t* txt_ptr) {
+    delete[] txt_ptr;
 }
