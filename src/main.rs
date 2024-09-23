6f2f5ed21cdcf65c365bee6ff49639bae4367657
Uwe Riegel <uriegel@hotmail.de>
2024-09-23 15:28:08 +0200

    Calling C++ and MessageBoxW from rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![windows_subsystem = "windows"]
+
+extern crate libloading;
+
+use libloading::{Library, Symbol};
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        // TODO path for release dll
+        // TODO copy release dll to path
+        // TODO Load Release dll from resource
+        // TODO Macro std::include_bytes!
+        // TODO Macro std::include_str!
+        let lib = Library::new("C:/Projekte/webview-app/WebViewApp/x64/Debug/WebViewApp.dll").expect("Failed to load DLL");
+        let func: Symbol<unsafe extern fn() -> ()> =
+            lib.get(b"Test1").expect("Failed to load function");
+
+        // Call the function
+        func();
+    }
 }
