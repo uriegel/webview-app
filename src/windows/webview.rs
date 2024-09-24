e7b908b16cef669eea731eb4e602b1fbfc16b814
Uwe Riegel <uriegel@hotmail.de>
2024-09-24 16:37:51 +0200

    //

--- src/windows/webview.rs
+++ src/windows/webview.rs
@@ -1,6 +1,6 @@
 // extern crate libloading;
 
-use core::slice;
+//use core::slice;
 use libloading::{Library, Symbol};
 use std::fs;
 
@@ -36,25 +36,25 @@ impl WebView {
     pub fn run(&self)->u32 {
         unsafe {
             let run: Symbol<unsafe extern fn() -> u32> = self.lib.get(b"Run").expect("Failed to load function 'Run'");
-            run();
+            let res = run();
 
-            let func: Symbol<unsafe extern fn(*const u16) -> *const u16> =
-                self.lib.get(b"Test1").expect("Failed to load function");
+            // let func: Symbol<unsafe extern fn(*const u16) -> *const u16> =
+            //     self.lib.get(b"Test1").expect("Failed to load function");
             
-            let wc = utf_16_null_terminiated("Das ist ein sehr schöner Messagebox-Text");
-            let text_ptr = func(wc.as_ptr());
-            let strlen: Symbol<unsafe extern fn(*const u16) -> usize> =
-                self.lib.get(b"Strlen").expect("Failed to load function");
-            let bytes = slice::from_raw_parts(text_ptr, strlen(text_ptr));
-            let bytes: Vec<u16> = Vec::from(bytes);
-            let text = String::from_utf16_lossy(&bytes);
-            let wc = utf_16_null_terminiated(&text);
-            func(wc.as_ptr());
-            let free: Symbol<unsafe extern fn(*const u16) -> ()> =
-                self.lib.get(b"Free").expect("Failed to load function");
-            free(text_ptr);
+            // let wc = utf_16_null_terminiated("Das ist ein sehr schöner Messagebox-Text");
+            // let text_ptr = func(wc.as_ptr());
+            // let strlen: Symbol<unsafe extern fn(*const u16) -> usize> =
+            //     self.lib.get(b"Strlen").expect("Failed to load function");
+            // let bytes = slice::from_raw_parts(text_ptr, strlen(text_ptr));
+            // let bytes: Vec<u16> = Vec::from(bytes);
+            // let text = String::from_utf16_lossy(&bytes);
+            // let wc = utf_16_null_terminiated(&text);
+            // func(wc.as_ptr());
+            // let free: Symbol<unsafe extern fn(*const u16) -> ()> =
+            //     self.lib.get(b"Free").expect("Failed to load function");
+            // free(text_ptr);
+            res
         }
-        0
     }
 }
 
