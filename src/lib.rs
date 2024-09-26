09ba65d5cc74152e4c27d9eed07323cbc4d9d3fe
Uwe Riegel <uriegel@hotmail.de>
2024-09-26 18:31:09 +0200

    TODO Windows

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,16 @@ pub mod webview;
 mod bounds;
 mod callbacks;
 
-// TODO https://stackoverflow.com/questions/46373028/how-to-release-a-beta-version-of-a-crate-for-limited-public-testing
+// TODO In VisualStudio sln, make a script to copy app.dll and loader dll (debug AND release) to an assets directory
+// in TOML:
+// [package]
+// # ...
+//
+// # Include additional files when packaging the crate
+// include = ["assets/*"]
+//
+// in src code:
+// pub const CONFIG_BYTES: &'static [u8] = include_bytes!("assets/debug/apploader.dll");
+
 // TODO Windows ICON: build.rs: make resource file with icon 1, version 
 // TODO Macro std::include_str!
