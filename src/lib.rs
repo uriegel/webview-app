648b60099e0836eb6e265bf5c44cae261d98a49b
uriegel <uriegel@hotmail.de>
2024-09-28 23:12:23 +0200

    TODO

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,5 @@ mod bounds;
 mod params;
 mod content_type;
 
-// TODO Test webviewapp with released webview_app with custom resources
 // TODO Windows ICON: build.rs: make resource file with icon 1, version 
 
