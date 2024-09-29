33080f21f044e90916b944709afcb232211f0948
uriegel <uriegel@hotmail.de>
2024-09-29 15:10:00 +0200

    TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,12 @@ mod content_type;
 mod javascript;
 mod html;
 
+// TODO Windows Check alternating img from res request
 // TODO Windows Devtools WebviewRequest
+// TODO Webview JsonRequests
+// TODO react test site debug: JsonRequests CORS?
 // TODO Windows Headerbar 
 // TODO DragDrop
 // TODO Windows ICON: build.rs: make resource file with icon 1, version 
+// TODO Simple webserver with range for videos
 
