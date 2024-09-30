5be0511243f0144d14f64d24740f58b916875551
uriegel <uriegel@hotmail.de>
2024-09-30 20:49:52 +0200

    TODO

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,10 @@ mod content_type;
 mod javascript;
 mod html;
 
-// TODO Linux javascript measure time postmessage from js and answer back
-// TODO Windows send back to js a very large json object (and measure time)
 // TODO Linux send back to js a very large json object (and measure time)
-// TODO Windows res and req scheme
+// TODO Windows send back to js a very large json object (and measure time)
 // TODO runtime for windows and linux
-// TODO react test site debug: JsonRequests CORS?
+// TODO Windows res and req scheme
 // TODO Windows Headerbar 
 // TODO DragDrop
 // TODO Windows ICON: build.rs: make resource file with icon 1, version 
