2c75a08e05a161112a8b6abd538a626e66a3840f
uriegel <uriegel@hotmail.de>
2024-10-08 17:26:42 +0200

    react-test

---
+++ examples/react-test.rs
@@ -0,0 +1,82 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Allows console to show up in debug build but not release build.
+
+use std::{thread, time::Duration};
+
+use serde::{Deserialize, Serialize};
+use webview_app::{application::Application, request::{self, request_blocking, Request}, webview::WebView};
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Input {
+    pub text: String,
+    pub id: i32
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Output {
+    pub text: String,
+    pub email: String,
+    pub number: i32
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Outputs {
+    pub outputs: Vec<Output>
+}
+
+fn on_activate(app: &Application)->WebView {
+    let webview = WebView::builder(app)
+        .title("Requests 👍".to_string())
+        .save_bounds()
+        .devtools(true)
+        .debug_url("http://localhost:5173/".to_string())
+        .default_contextmenu_disabled()
+        .build();
+    
+    webview.connect_request(|request, id, cmd: String, json| {
+        match cmd.as_str() {
+            "cmd1" => cmd1(request, id, json),
+            "cmd2" => cmd2(request, id),
+            _ => {}
+        }
+        true
+    });
+    webview
+}
+
+fn main() {
+    Application::new("de.uriegel.hello")
+    .on_activate(on_activate)
+    .run();
+}
+
+fn cmd1(request: &Request, id: String, json: String) {
+    request_blocking(request, id, move || {
+        let input: Input = request::get_input(&json);
+        let res = Output {
+            email: "uriegel@hotmail.de".to_string(),
+            text: input.text,
+            number: input.id + 1,
+        };
+        request::get_output(&res)
+    })
+}
+
+fn cmd2(request: &Request, id: String) {
+    request_blocking(request, id, move || {
+        let five_seconds = Duration::from_secs(5);
+        thread::sleep(five_seconds);
+
+        let res = Output {
+            email: "uriegel@hotmail.de".to_string(),
+            text: "Return fom cmd2  sd fd fdsf dsfdsg fdg dfg dfgdfgfdgdfgdfgdffdg dfg dfg dfgdfg dfg dfgdfg dfg dfg".to_string(),
+            number: 222,
+        };
+        request::get_output(&res)
+    })
+}
+
+ 
---
+++ examples/react-web/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  	return (
+		<>
+			<h1>Vite + React</h1>
+			<p>
+				Ein Test
+			</p>
+    	</>
+  	)
+}
+
+export default App
