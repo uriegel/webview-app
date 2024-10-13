786347dd2265053a9ea2e29542354756edc62d53
uriegel <uriegel@hotmail.de>
2024-10-13 09:59:10 +0200

    Video in custom resources

--- webroots/custom_resources/scripts/script.js
+++ webroots/custom_resources/scripts/script.js
@@ -1,6 +1,7 @@
 console.log("script loaded")
 const btn1 = document.getElementById("button")
 const btn2 = document.getElementById("button2")
+const btn3 = document.getElementById("button3")
 const btnDevTools = document.getElementById("buttonDevTools")
 const dragzone = document.getElementById("dragzone")
 
@@ -41,6 +42,12 @@ btn2.onclick = async () => {
     const t1 = performance.now()
     const tdiff = t1 - t0
     console.log(`${tdiff}`, "cmd2", res)
+}
+
+btn3.onclick = () => {
+    const video = document.getElementById("video")
+    video.classList.remove("hidden")
+    video.src="http://roxy:8080/media/video/2010.mp4"
 }
 
 dragzone.onmousedown = () => WebView.startDragFiles([
