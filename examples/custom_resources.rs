c4c78bbab0d53ff2fb56495d28e76ba8d88e6390
uriegel <uriegel@hotmail.de>
2024-12-04 11:35:06 +0100

    No requests

--- examples/custom_resources.rs
+++ examples/custom_resources.rs
@@ -2,23 +2,7 @@
 // Allows console to show up in debug build but not release build.
 
 use include_dir::include_dir;
-use serde::{Deserialize, Serialize};
 use webview_app::{application::Application, webview::WebView};
-
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Input {
-    pub text: String,
-    pub id: i32
-}
-
-#[derive(Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Output {
-    pub text: String,
-    pub email: String,
-    pub number: i32
-}
 
 fn on_activate(app: &Application)->WebView {
     let webview = WebView::builder(app)
