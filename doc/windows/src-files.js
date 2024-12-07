e1a96bba00730cc40e74799dc71e4fb990a7798b
Uwe Riegel <uriegel@hotmail.de>
2024-12-07 11:42:46 +0100

    New windows version

--- doc/windows/src-files.js
+++ doc/windows/src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["webview_app",["",[["windows",[],["application.rs","framewindow.rs","mod.rs","webview.rs"]]],["application.rs","bounds.rs","content_type.rs","html.rs","javascript.rs","lib.rs","params.rs","request.rs","webview.rs"]]]\
+["webview_app",["",[["windows",[],["application.rs","appmessage.rs","framewindow.rs","mod.rs","webview.rs"]]],["application.rs","bounds.rs","content_type.rs","html.rs","javascript.rs","lib.rs","params.rs","request.rs","webview.rs"]]]\
 ]'));
 createSrcSidebar();
